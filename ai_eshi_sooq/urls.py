7e952ead20f9aedcadf65702723517c075ae2433
azez-alhoot <azezalhoot82@yahoo.com>
2020-10-18 11:52:29 +0300

    edit-back

--- ai_eshi_sooq/urls.py
+++ ai_eshi_sooq/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/post/',include('posts.urls')),
-    path('auth/',obtain_auth_token),
+    # path('auth/',obtain_auth_token),
     # path('api/v1/posts/',include('posts.urls')),
 ]
